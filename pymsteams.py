decb2a6c52507883c119f63a08c775f1405c1210
Veach,Ryan <Ryan.Veach@Cerner.com>
2017-04-20 18:57:28 -0500

    corrections

--- pymsteams.py
+++ pymsteams.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# https://github.com/rveachkc/pymsteams/
+
 import requests
 
 class section:
@@ -7,30 +9,30 @@ class section:
 	def title(self, stitle):
 		# title of the section
 		self.payload["title"] = stitle
-		
+
 	def activityTitle(self, sactivityTitle):
 		# Title of the event or action. Often this will be the name of the "actor".
 		self.payload["activityTitle"] = sactivityTitle
-		
-	def activitySubtitle(self sactivitySubtitle):
+
+	def activitySubtitle(self, sactivitySubtitle):
 		# A subtitle describing the event or action. Often this will be a summary of the action.
 		self.payload["activitySubtitle"] = sactivitySubtitle
-		
+
 	def activityImage(self, sactivityImage):
 		# URL to image or a data URI with the base64-encoded image inline.
 		# An image representing the action. Often this is an avatar of the "actor" of the activity.
 		self.payload["activityImage"] = sactivityImage
-		
+
 	def activityText(self, sactivityText):
 		# A full description of the action.
 		self.payload["activityText"] = sactivityText
-		
+
 	def addFact(self, factname, factvalue):
 		if "facts" not in self.payload.keys():
 			self.payload["facts"] = [ { factname : factvalue } ]
 		else:
 			self.payload["facts"].append({ factname : factvalue })
-			
+
 	def addImage(self, simage, stitle=None):
 		if "images" not in self.payload.keys():
 			self.payload["images"] = []
@@ -39,10 +41,10 @@ class section:
 		if stitle:
 			imobj["title"] = stitle
 		self.payload["images"].append(imobj)
-		
+
 	def text(self, stext):
 		self.payload["text"] = stext
-		
+
 	def linkbutton(self, buttontext, buttonurl):
 		self.payload["potentialAction"] = [
 			{
@@ -55,29 +57,29 @@ class section:
 
 	def disablemarkdown():
 		self.payload["markdown"] = False
-		
+
 	def enablemarkdown():
 		self.payload["markdown"] = True
-		
+
 	def __init__(self, sslug):
 		self.slug=sslug
 		self.payload = {}
-		
+
 
 class connectorcard:
 
 	def text(self, mtext):
 		self.payload["text"] = mtext
-		
+
 	def title(self, mtitle):
 		self.payload["title"] = mtitle
-		
+
 	def color(self, mcolor):
 		if mcolor.lower() == "red":
 			self.payload["themeColor"] = "E81123"
 		else:
 			self.payload["themeColor"] = themeColor
-		
+
 	def linkbutton(self, buttontext, buttonurl):
 		self.payload["potentialAction"] = [
 			{
@@ -87,28 +89,28 @@ class connectorcard:
 			"target" : [ buttonurl ]
 			}
 		]
-		
+
 	def newhookurl(self, nhookurl):
 		self.hookurl = nhookurl
-		
-	def printme():
+
+	def printme(self):
 		print("hookurl: %s" % self.hookurl)
 		print("payload: %s" % self.payload)
-		
+
 	def send(self):
 		headers = {"Content-Type":"application/json"}
 		r = requests.post(self.hookurl, json=self.payload, headers=headers)
-		
+
 		if r.status_code == requests.codes.ok:
 			return True
 		else:
 			print(r.text)
 			return False
-		
+
 	def __init__(self, hookurl):
 		self.payload = {}
 		self.hookurl = hookurl
-	
+
 def formaturl(display, url):
 	mdurl = "[%s](%s)" % (display, url)
 	return mdurl
