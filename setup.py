0ee225e93d2d385858716a61d8d90f8ee0ac0521
Veach,Ryan <Ryan.Veach@Cerner.com>
2017-04-20 17:40:43 -0500

    initial commit
    adding work in progress

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'pymsteams',
+  packages = ['pymsteams'],
+  version = '0.1',
+  description = 'Format messages and post to Microsoft Teams.',
+  author = 'Ryan Veach',
+  author_email = 'rveach@gmail.com',
+  url = 'https://github.com/rveachkc/pymsteams',
+  download_url = 'https://github.com/rveachkc/pymsteams/archive/0.1.tar.gz',
+  keywords = ['Microsoft', 'Teams'], # arbitrary keywords
+  classifiers = [],
+  install_requires=['requests'],
+)
