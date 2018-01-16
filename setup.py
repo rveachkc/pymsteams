333245d397d45d377bef91d3ea104ac1a7d02ff9
Ryan Veach <rveach@gmail.com>
2018-01-16 11:42:18 -0600

    version 0.1.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pymsteams',
   packages = ['pymsteams'],
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'Format messages and post to Microsoft Teams.',
   author = 'Ryan Veach',
   author_email = 'rveach@gmail.com',
   url = 'https://github.com/rveachkc/pymsteams',
-  download_url = 'https://github.com/rveachkc/pymsteams/archive/0.1.2.tar.gz',
+  download_url = 'https://github.com/rveachkc/pymsteams/archive/0.1.3.tar.gz',
   keywords = ['Microsoft', 'Teams'], # arbitrary keywords
   classifiers = [],
   install_requires=['requests'],
