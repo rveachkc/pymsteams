c4ff76bea3b3f93284dd2323c3d7114fe93382f4
Ryan Veach <rveach@gmail.com>
2018-11-02 13:23:54 -0500

    try circleci

--- setup.py
+++ setup.py
@@ -1,14 +1,61 @@
-from distutils.core import setup
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import setup
+from setuptools.command.install import install
+
+VERSION = "0.1.5"
+
+def readme():
+    """ print long description """
+    with open('README.md') as f:
+        return r.read()
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+
 setup(
-  name = 'pymsteams',
-  packages = ['pymsteams'],
-  version = '0.1.5',
-  description = 'Format messages and post to Microsoft Teams.',
-  author = 'Ryan Veach',
-  author_email = 'rveach@gmail.com',
-  url = 'https://github.com/rveachkc/pymsteams',
-  download_url = 'https://github.com/rveachkc/pymsteams/archive/0.1.5.tar.gz',
-  keywords = ['Microsoft', 'Teams'], # arbitrary keywords
-  classifiers = [],
-  install_requires=['requests'],
+    name="pymsteams",
+    version=VERSION,
+    description="Format messages and post to Microsoft Teams.",
+    long_description=readme(),
+    url="https://github.com/rveachkc/pymsteams",
+    author="Ryan Veach",
+    author_email="rveach@gmail.com",
+    license="Apache",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords=['Microsoft', 'Teams'],
+    packages=['pymsteams'],
+    install_requires=[
+        'requests==2.18.4',
+    ],
+    python_requires='>=3',
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
