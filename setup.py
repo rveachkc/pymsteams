c692c1c228d1e1714b2092ab5feeca0c5d3e2018
Ryan Veach <rveach@gmail.com>
2020-06-08 09:47:44 -0500

    increment to 0.1.13

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "0.1.12"
+VERSION = "0.1.13"
 
 def readme():
     """ print long description """
