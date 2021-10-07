d98fd42e09d993423c589bbf184783f52fda5b66
Ryan Veach <rveach@gmail.com>
2021-10-07 08:27:09 -0500

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "0.1.15"
+VERSION = "0.1.16"
 
 def readme():
     """ print long description """
